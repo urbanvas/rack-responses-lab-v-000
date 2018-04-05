c2f6f6c96c49abd598a6b2577c1e5ca5234684a1
Neil Seejoor <nvseejoor@gmail.com>
2018-04-05 23:19:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     time = Time.new
-    binding.pry
+    # binding.pry
     if time.hour >= 12
       # binding.pry
       resp.write "Afternoon\n"
